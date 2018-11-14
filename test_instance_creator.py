923598702d6ea32a40ff805eccc1f1532c652b21
matti <hellwig@matti-lappi>
2018-11-14 21:29:46 +0100

    clean up

--- test_instance_creator.py
+++ test_instance_creator.py
@@ -1,12 +1,24 @@
-def create_instance(number_nodes, edge_vector):
+def edge_vector_length(number_nodes):
+    return int(number_nodes * (number_nodes - 1) / 2)
+
+
+def create_instance(number_of_nodes, edge_vector):
+    if pow(2, edge_vector_length(number_of_nodes)) - 1 < edge_vector:
+        raise ValueError("edge vector too long for number of nodes in graph")
     result = []
-    for i in range(1, number_nodes):
-        for j in range(i + 1, number_nodes):
-            if edge_vector % 2 == 1:
+    edge_vector_rest = edge_vector
+    for i in range(1, number_of_nodes + 1):
+        for j in range(i + 1, number_of_nodes + 1):
+            if edge_vector_rest % 2 == 1:
                 result.append((i,j))
-                edge_vector = edge_vector >> 1
+            edge_vector_rest = edge_vector_rest >> 1
     return result
 
-example_graph = create_instance(5, 8)
-complete_graph = create_instance(6, pow(2,6) - 1)
-print(str(complete_graph))
+
+# example usages
+# example_graph = create_instance(4, 15)
+# print("example 1: " + str(example_graph))
+
+# number_of_nodes = 6
+# complete_graph = create_instance(number_of_nodes, pow(2, edge_vector_length(number_of_nodes)) - 1)
+# print("example 2: " + str(complete_graph))
