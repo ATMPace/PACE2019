242e9ec34d011f5dcb416f7584c63336dd86331d
ATMPace <atm@pace>
2018-11-19 21:21:21 +0100

    Random graph as input for testing.

--- tests.py
+++ tests.py
@@ -1,8 +1,9 @@
-from test_instance_creator import create_instance
+from test_instance_creator import *
 from naive_formulation import solve_exactly
 from random import randint
 
-random_edges = randint(0, pow(2, 100 * 199)-1)
+number_of_nodes = 200
+random_edges = randint(0, pow(2, edge_vector_length(number_of_nodes)) - 1)
 print("random edge vector is " + str(random_edges))
-graph = create_instance(200, random_edges)
+graph = create_instance(number_of_nodes, random_edges)
 print(solve_exactly(graph))
