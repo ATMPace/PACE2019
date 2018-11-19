e0514371424cf32d8d750bf4ce6d5b18c6e0e744
ATMPace <atm@pace>
2018-11-19 20:49:55 +0100

    Corrections naive formulation.

--- naive_formulation.py
+++ naive_formulation.py
@@ -4,19 +4,21 @@ from gurobipy import *
 
 def add_variable_to_model_if_necessary(v, model, added):
     if added.__contains__(v):
-        return
+        return model.getVarByName(str(v))
     added.add(v)
-    model.addVar(obj=1, vtype=GRB.BINARY, name=str(v))
-    return
+    added_var = model.addVar(obj=1, vtype=GRB.BINARY, name=str(v))
+    model.update()
+    return added_var
 
 
 def read_solution(model):
-    result = set()
+    result = []
     vars_in_model = model.getVars()
 
     for var in vars_in_model:
         if var.X == 1:
-            result.add(var.name)
+            result.append(var.VarName)
+    result.sort()
     return result
 
 
@@ -24,19 +26,18 @@ def add_constraints_to_model(edges, model):
     added = set()
     for edge in edges:
         (u, v) = edge
-        add_variable_to_model_if_necessary(u, model, added)
-        add_variable_to_model_if_necessary(v, model, added)
-        var_u = model.getVarByName(str(u))
-        var_v = model.getVarByName(str(v))
-        model.addConstr(vars.sum(var_u, var_v) >= 1)
+        var_u = add_variable_to_model_if_necessary(u, model, added)
+        var_v = add_variable_to_model_if_necessary(v, model, added)
+        model.addConstr(var_u + var_v >= 1)
 
 
 def solve_exactly(edgelist):
     initial_solution = cover_greedily(edgelist)
     model = Model("vertex_cover_IP")
     model.setParam("MIPGap", 0)
+    add_constraints_to_model(edgelist, model)
     for initial_var in initial_solution:
         model.getVarByName(str(initial_var)).start = 1
-    add_constraints_to_model(edgelist, model)
+    model.update()
     model.optimize()
     return read_solution(model)
